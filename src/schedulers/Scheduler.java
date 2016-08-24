89022884bcff9b346155cce4c21d7002d70c4518
vNeon <vnio666@aucklanduni.ac.nz>
2016-08-25 11:09:32 +1200

    added comments

--- src/schedulers/Scheduler.java
+++ src/schedulers/Scheduler.java
@@ -6,10 +6,29 @@ import dag.Dag;
 
 import node.Node;
 
+/**
+ * Scheduler is an interface which methods should be implemented by all
+ * schedulers
+ * 
+ */
 public interface Scheduler extends Runnable {
-	
+	/**
+	 * Method which runs an algorithm for producing an optimal schedule
+	 */
 	public void schedule();
+
+	/**
+	 * This method returns an optimal schedule
+	 * 
+	 * @return schedule
+	 */
 	public ArrayList<Node> getSchedule();
+
+	/**
+	 * Gives the scheduler a Graph Stream Graph for visualization purposes
+	 * 
+	 * @param visual
+	 */
 	public void setVisual(Dag visual);
-	
+
 }
